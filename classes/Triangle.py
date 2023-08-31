f7b3574cf7948dba3239026dfa19c46e1845b7f0
Dheeraj <134566303+upraity@users.noreply.github.com>
2023-08-31 11:09:23 +0530

    Update Triangle.py
    updated

--- classes/Triangle.py
+++ classes/Triangle.py
@@ -1,13 +1,10 @@
 class triangle:
-    def _init_(tri, base, height):
+    def __init__(tri, base, height):
         tri.base   = base
         tri.height = height
-        tri.side1  = side1
-        tri.side2  = side2
-        tri.side3  = side3
 
-    def _repr_(tri):
-        return "Triangle base: " + str(tri.base) + ", height: " + str(tri.height) + "a:" + str(tri.side1) + ",b:" + str(tri.side2) + ",c:" + str(tri.side1)
+    def __repr__(tri):
+        return "Triangle base: " + str(tri.base) + ", height: " + str(tri.height) 
 
     def set_base(tri, new_base):
         tri.base = new_base
@@ -15,41 +12,12 @@ class triangle:
     def set_height(tri, new_height):
         tri.height = new_height
 
-    def set_side1(tri, new_side1):
-        tri.side1 = new_side1
-
-    def set_side2(tri, new_side2):
-        tri.side2 = new_side2
-
-    def set_side3(tri, new_Side3):
-        tri.side3 = new_side3
-
     def base(tri):
         return tri.base
 
     def height(tri):
         return tri.height
 
-    def side1(tri):
-        return tri.side1
-
-    def side2(tri):
-        return tri.side2
-
-    def side3(tri):
-        return tri.side3
-
     def area(tri):
         return (tri.base * tri.height * 0.5)
 
-    def perimeter(tri):
-        return tri.side1 + tri.side2 + tri.side3
-
-    def is_equalateral_triangle(tri):
-        return tri.side1 == tri.side2 == tri.side3 == a
-
-    def area_equalateral_tri(tri):
-        return (a*a*1.731)/4
-
-    def perimeter_equalateral_tri(tri):
-        return 3*a
